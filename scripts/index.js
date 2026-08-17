02a587d85b6cb4facdba832a542a97ea3b7edd52
fxhxyz4 <fxhxyz8@gmail.com>
2026-08-17 01:30:44 +0000

    Deploy design

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+const downloadGroups = {
+  gradient: [
+    './img/faust-icon-gradient.png',
+    './img/faust-logo-gradient-white.png',
+    './img/faust-logo-gradient-black.png',
+  ],
+
+  white: ['./img/faust-icon-white.png', './img/faust-logo-white.png', './img/faust-logo-gradient-white.png'],
+
+  black: ['./img/faust-icon-black.png', './img/faust-logo-gradient-black.png'],
+};
+
+document.querySelectorAll('[data-download-group]').forEach(link => {
+  link.addEventListener('click', async event => {
+    event.preventDefault();
+
+    const files = downloadGroups[link.dataset.downloadGroup];
+
+    if (!files) return;
+
+    link.classList.add('is-downloading');
+
+    for (const file of files) {
+      const download = document.createElement('a');
+
+      download.href = file;
+      download.download = '';
+      download.style.display = 'none';
+
+      document.body.appendChild(download);
+      download.click();
+      download.remove();
+
+      await new Promise(resolve => setTimeout(resolve, 250));
+    }
+
+    link.classList.remove('is-downloading');
+  });
+});
